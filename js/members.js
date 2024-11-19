5802cf7a10bb45a4429531bf84d31829a1d00634
KMD <71997429+ArnurSh@users.noreply.github.com>
2024-11-19 23:14:47 +0500

    Update 0.6
    Members:
    -Cursor Sys

--- js/members.js
+++ js/members.js
@@ -5,3 +5,11 @@ function on(numb) {
 function off(numb) {
     document.getElementById("overlay" + numb).style.display = "none";
 }
+
+function Cursor(){
+    const cursor = document.getElementById('cursor');
+    setTimeout(() => {
+        cursor.remove();
+    }, 3000);
+}
+Cursor();
